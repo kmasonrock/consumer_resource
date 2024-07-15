4c1d6893e1f8be706e42f68e14263ef80f19de7d
kmasonrock <72939370+kmasonrock@users.noreply.github.com>
2024-07-15 13:10:09 -0400

    Changes to Model Stat Tests
    I just added breakdown of the multiple extinction statistic. Namely, whether the multiple extinction includes the target species or not. If there are multiple extinctions that include the target, it's likely that we have too greedy an algorithm and are enforcing a scorched earth policy where we don't care who dies as long as the one we want to die dies too.

--- model_stat_tests.jl
+++ model_stat_tests.jl
@@ -1,6 +1,5 @@
 using ModelingToolkit, OrdinaryDiffEq, Symbolics
 using Glob, JLD2
-using PyPlot
 using LinearAlgebra, StatsBase
 using JuMP
 import Ipopt, HSL_jll
@@ -13,33 +12,57 @@ function model_stats(dir)
     local end_state = []
     local no_extinct = 0
     local ne_file = []
-    local multi_extinct = 0
-    local me_file = []
+    local multi_extinct_nt = 0
+    local multi_extinct_tg = 0
+    local me_nt_file = []
+    local me_tg_file = []
     local one_extinct = 0
     local oe_file = []
     for file in files
         f = jldopen(file)
         termination = vcat(termination,f["term_status"])
         end_state = vcat(end_state,f["end_state"])
+        target = parse(Int64, string(split(split(file,"_")[end],".")[1]))
 
-        sol = f["best_sol"].u
+        sol = f["best_sol"]
+        num_alive = sol .>= -8
 
-        num_alive = sol[end] .>= -8
-
-        if sum(num_alive) == length(sol[end])
+        if sum(num_alive) == length(sol)
             no_extinct += 1
             ne_file = vcat(ne_file, [file])
 
-        elseif sum(num_alive) == (length(sol[end]) - 1)
+        elseif sum(num_alive) == (length(sol) - 1)
             one_extinct += 1
             oe_file = vcat(oe_file, [file])
         else
-            multi_extinct += 1
-            me_file = vcat(me_file, [file])
+            mask = findall(x -> x == 1, num_alive)
+            if target in mask
+                multi_extinct_tg += 1
+                me_tg_file = vcat(me_tg_file, [file])
+            else
+                multi_extinct_nt += 1
+                me_nt_file = vcat(me_nt_file, [file])
+            end
         end
     end
+
+    jldsave(dir*"_extinction_stat_files.jld2"; me_nt = me_nt_file, me_tg = me_tg_file, oe = oe_file, ne = ne_file, counts = [multi_extinct_nt, multi_extinct_tg, one_extinct, no_extinct])
     @infiltrate
 end
+#hill_w0_h1_fail:
+#   me = 84 (88.4%)
+#   oe = 7
+#   ne = 4
+
+#hill_w0_h2_fail:
+#   me = 90 (60.4%)
+#   oe = 4
+#   ne = 55
+
+#w0-05_h1-2_fail
+#   me = 92 (98.9%)
+#   oe = 0
+#   ne = 1
 
 
 #Greedy Algorithm 3.0
